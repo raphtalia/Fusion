4f33474cbe11a7eec69dee42d1c52b93d3681e65
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-08-28 18:44:24 -0500

    Added more tests to States

--- test/State/State.spec.lua
+++ test/State/State.spec.lua
@@ -20,4 +20,44 @@ return function()
 		state:set(State)
 		expect(state:get()).to.equal(State)
 	end)
+
+	it("should error if trying to use shorthand table methods on a non-table state", function()
+		local state = State()
+
+		expect(function()
+			state:insert(1)
+		end).to.throw()
+
+		expect(function()
+			state:remove(1)
+		end).to.throw()
+
+		expect(function()
+			state:find(1)
+		end).to.throw()
+
+		expect(function()
+			state:sort(1)
+		end).to.throw()
+	end)
+
+	it("should be able to use shorthand table methods on a table state", function()
+		local state = State({})
+
+		state:insert(1)
+		expect(state:get()[1]).to.equal(1)
+		expect(state:find(1)).to.equal(1)
+
+		state:remove(1)
+		expect(state:get()[1]).to.equal(nil)
+		expect(state:find(1)).to.equal(nil)
+
+		state:set({1, 2, 3})
+		state:sort(function(a, b)
+			return a > b
+		end)
+		expect(state:get()[1]).to.equal(3)
+		expect(state:get()[2]).to.equal(2)
+		expect(state:get()[3]).to.equal(1)
+	end)
 end
